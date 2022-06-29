4366a2b3b9ada819197069706f10793fa7cd04b5
Nicolas Aimar <Nicoaimar@MacBook-Pro.fibertel.com.ar>
2022-06-29 15:18:50 -0300

    Second Commit

--- sesion-6-ejercicio-1.py
+++ sesion-6-ejercicio-1.py
@@ -11,20 +11,19 @@
     
 # Por último, tendrás que crear un objeto de la clase Coche y mostrarlo por consola.
     
-class Vehiculo:
-        color = input('Ingresa el color: ')
-        ruedas = input('Ingresa el número de ruedas: ')
-        puertas = input('Ingresa el número de puertas: ')
+
+class Vehiculo():
+    def __init__(self, color, ruedas, puertas):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
 
 class Coche(Vehiculo):
-    velocidad = input('Ingresa la velocidad: ')
-    cilindrada = input('Ingresa la cilindrada: ')
+    def __init__(self, color, ruedas, puertas, velocidad, cilindrada):
+        super().__init__(color, ruedas, puertas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+    
+cocheUno = Coche('Azul', 4, 3, 150, 2.0)
 
-
-amarok = Coche()
-
-print('Color: ', amarok.color)
-print('Ruedas: ', amarok.ruedas)
-print('Puertas: ', amarok.puertas)
-print('Velocidad: ', amarok.velocidad)
-print('Cilindrada: ', amarok.cilindrada)
+print('Color: ', cocheUno.color,' Cantidad de Ruedas: ', cocheUno.ruedas,' Cantidad de Puertas: ', cocheUno.puertas,' Velocidad: ', cocheUno.velocidad,' Cilindrada: ', cocheUno.cilindrada)
